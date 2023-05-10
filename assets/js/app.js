7fd8ff5f2d2976d5470fbf94004b9ff5457aec81
lauberna <laureanoberna2002@gmail.com>
2023-05-09 22:38:00 -0300

    13

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,12 @@
 /* import { carousel } from "../../modules/Carousel.js"; */
 import navBar from "../../modules/primary/navBar.js";
-import validateForm from "../../modules/primary/validateForm.js";
+/* import validateForm from "../../modules/primary/validateForm.js"; */
 import { sections } from "../../modules/primary/body.js";
 import { createElements } from "../../modules/socialMedia.js";
 
 function appInit() {
   navBar();
   sections();
-  validateForm();
+/*   validateForm(); */
 }
 appInit();
